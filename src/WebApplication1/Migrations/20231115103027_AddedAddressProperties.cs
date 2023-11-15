98dc2f2d1e33dd7b6b6439d4426bf9a422c139e8
Holger Haack <holger.haack@t-online.de>
2023-11-15 11:28:39 +0100

    feat: added more properties to address

---
+++ src/WebApplication1/Migrations/20231115103027_AddedAddressProperties.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAddressProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HouseNumber",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "HouseNumber",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Addresses");
+        }
+    }
+}
--- src/WebApplication1/Models/Address.cs
+++ src/WebApplication1/Models/Address.cs
@@ -2,6 +2,10 @@ namespace WebApplication1.Models;
 
 public class Address
 {
-   public int Id { get; set; }
-   public string City { get; set; }
+    public int Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string? PostalCode { get; set; }
+    public string Street { get; set; }
+    public string HouseNumber { get; set; }
 }
