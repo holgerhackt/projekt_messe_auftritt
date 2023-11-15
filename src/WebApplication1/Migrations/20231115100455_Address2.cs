b6df45151058875139be5a429c871a04770d8324
HookedBehemoth <luis@scheurenbrand.me>
2023-11-15 11:09:36 +0100

    Add Address table with link from Image

---
+++ src/WebApplication1/Migrations/20231115100455_Address2.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Address2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Address_AddressId",
+                table: "Images");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Address",
+                table: "Address");
+
+            migrationBuilder.RenameTable(
+                name: "Address",
+                newName: "Addresses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Addresses_AddressId",
+                table: "Images",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Addresses_AddressId",
+                table: "Images");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "Address");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Address",
+                table: "Address",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Address_AddressId",
+                table: "Images",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ src/WebApplication1/Models/Address.cs
@@ -0,0 +1,7 @@
+namespace WebApplication1.Models;
+
+public class Address
+{
+   public int Id { get; set; }
+   public string City { get; set; }
+}
