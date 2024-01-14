248f55f8ef3140260414e472ef43b1bcd0c172a7
Holger Haack <holger.haack@t-online.de>
2024-01-14 20:22:58 +0100

    style: added the AutoMapper.Extensions.Microsoft.DependencyInjection for uniform usage of dependency injection

--- src/ApiServer/Program.cs
+++ src/ApiServer/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ImageContext>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
